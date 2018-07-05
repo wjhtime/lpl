02a99c0fa0350c26e143334f28d9b581daf8ca22
jh-wen <jh-wen@haihangyun.com>
2018-07-05 11:41:50 +0800

    lxml更换为bs4

--- lpl/webSearch.py
+++ lpl/webSearch.py
@@ -1,6 +1,8 @@
 import requests
 from lxml import etree
 import prettytable as pt
+from bs4 import BeautifulSoup
+from termcolor import colored
 
 searchUrl = "https://www.baidu.com/s?wd=lol {}"
 
@@ -10,15 +12,15 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
 }
 
+# 搜索功能
 def search(keyword):
     tb = pt.PrettyTable()
-    tb.field_names = ['id', '标题']
+    tb.field_names = [colored('id', 'blue'), colored('标题', 'blue')]
     response = requests.get(searchUrl. format(keyword), headers=headers)
-    for k, sel in enumerate(etree.HTML(response.text).xpath("//div[@class='result c-container ']")):
-        # print(etree.tostring(sel.xpath("h3/a")[0]))
-        # exit()
-        tb.add_row([k, sel.xpath("h3/a/text()")[0]])
-        tb.add_row(['', sel.xpath("h3/a/@href")[0]])
+
+    for k, sel in enumerate(BeautifulSoup(response.text, 'lxml').select("div[class='result c-container ']")):
+        tb.add_row([k, colored(sel.h3.a.get_text(), 'red')])
+        tb.add_row(['', colored(sel.h3.a.attrs['href'], 'yellow')])
         tb.add_row(['', ''])
 
     tb.align = 'l'
